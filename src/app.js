f427cd7e8ed74bed38474d1cdfc74ebf2e50235e
ysodki-maker <ysodki@virtualconnect.ma>
2026-04-01 16:39:29 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 const express     = require('express');
 const helmet      = require('helmet');
 const cors        = require('cors');
